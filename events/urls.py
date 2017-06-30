e0c4e130a901a9b85c0da0c082cc3025127fda03
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2017-06-30 12:52:42 +0300

    menu

--- events/urls.py
+++ events/urls.py
@@ -9,10 +9,10 @@ events_list='|'.join(events_list)
 
 urlpatterns = [
         url(r'^$', views.afisha, name='index'),
-        url(r'projects/$', views.index, name='index'),
+        url(r'projects/$', views.index, name='projects_index'),
         url(r'(?P<permalink>' + events_list + ')/', views.show_event, name='show_event'),
 #        url(r'projects/(?P<permalink>\w+)/', views.show_event, name='show_event'),
-        url(r'team/$', views.team, name='index'),
+        url(r'team/$', views.team, name='team'),
         url(r'team/(?P<nick>\w+)/$', views.show_guide, name='show_guide'),
         url(r'feedback/$', views.reviews, name='index'),
         url(r'contacts/$', pages.page, {"page": "contacts"}),
--- website/context_processors.py
+++ website/context_processors.py
@@ -1,6 +1,32 @@
 from website.slogans import slogans
 import random
+from django.urls import reverse
 
-def generic(request):
+def slogans_gen(request):
     slogan = random.choice(slogans)
     return {'slogan': slogan}
+
+def main_menu(request):
+    main_menu = [
+    {
+    "url":"/",
+    "title":"Афиша",
+    },
+    {
+    "url": reverse('projects_index'),
+    "title": "Наши проекты",
+    },
+    {
+    "url":reverse('team'),
+    "title":"Команда",
+    },
+    {
+    "url":"/contacts/",
+    "title":"Дружба и контакты",
+    },
+    {
+    "url":"/feedback/",
+    "title":"Отзывы",
+    },
+    ]
+    return {'main_menu':main_menu}
